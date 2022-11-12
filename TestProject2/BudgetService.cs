fd89308bb56b5ea0d11742e1f0b5c98bb004fa62
Joey Chen <jay.hatelove@gmail.com>
2022-11-12 22:45:33 +0800

    refactor: move Period class to independent file

--- TestProject2/BudgetService.cs
+++ TestProject2/BudgetService.cs
@@ -7,18 +7,6 @@ using System.Linq;
 
 namespace TestProject2
 {
-    public class Period
-    {
-        public Period(DateTime start, DateTime end)
-        {
-            Start = start;
-            End = end;
-        }
-
-        public DateTime End { get; private set; }
-        public DateTime Start { get; private set; }
-    }
-
     public class BudgetService
     {
         readonly IBudgetRepo _budget;
